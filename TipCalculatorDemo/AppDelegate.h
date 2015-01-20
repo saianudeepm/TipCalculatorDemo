3d9b55ef5e5401855c1d344a99df00814af7e74b
saianudeepm <sai.anudeep.m@gmail.com>
2015-01-20 14:04:52 -0800

    Initial Commit

---
+++ TipCalculatorDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculatorDemo
+//
+//  Created by Sai Anudeep Machavarapu on 1/20/15.
+//  Copyright (c) 2015 salome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TipCalculatorDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculatorDemo
+//
+//  Created by Sai Anudeep Machavarapu on 1/20/15.
+//  Copyright (c) 2015 salome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
